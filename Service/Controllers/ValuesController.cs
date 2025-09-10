e31078f6ca23e42f0be81365d2033bc611f91d08
foykon <foykon9@gmail.com>
2025-09-10 18:25:44 +0300

    Add project files.

---
+++ Service/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Service.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await Task.Delay(15000); 
+            return Ok(new string[] { "Furkan", "YILDIZ" });
+        }
+    }
+}
---
+++ Service2/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+HttpClient client = new();
+HttpResponseMessage result = await client.GetAsync("https://localhost:7080/api/values");
+
+if(result.IsSuccessStatusCode)
+{
+    string content = await result.Content.ReadAsStringAsync();
+    Console.WriteLine(content);
+}
+else
+{
+    Console.WriteLine("Error: " + result.StatusCode);
+}
