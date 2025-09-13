ae3297d55ecc5d4c7c3f971716669ac2e6708c50
foykon <foykon9@gmail.com>
2025-09-13 20:28:23 +0300

    basics of rabbitmq. stuied with publisher and consumer

---
+++ Consumer/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+// Creating Connection
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("...");
+
+// active connection and channel
+IConnection connection = await factory.CreateConnectionAsync();
+IChannel channel = await connection.CreateChannelAsync();
+
+// declare a queue
+await channel.QueueDeclareAsync(
+    queue: "example-queue",
+    exclusive: false // same as publisher 
+);
+
+// reading message from mq
+AsyncEventingBasicConsumer consumer = new AsyncEventingBasicConsumer(channel);
+await channel.BasicConsumeAsync(
+    queue: "example-queue",
+    false,
+    consumer
+);
+consumer.ReceivedAsync += (sender, e) =>
+{
+    // message from queue 
+    // e.body : gets message from queue s all data
+    // e.Body.Span or e.body.ToArray() : gets byte data
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+
+    return Task.CompletedTask; 
+};
+
+Console.ReadLine();
---
+++ Publisher/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+// creating connection
+ConnectionFactory factory  = new ConnectionFactory();
+factory.Uri = new Uri("...");
+
+// active connection and channel
+using IConnection connection = await factory.CreateConnectionAsync(); 
+using IChannel channel = await connection.CreateChannelAsync();
+
+// declare a queue
+await channel.QueueDeclareAsync(
+    queue: "example-queue",
+    exclusive: false
+);
+
+// publish a message
+// rabbitmq accepts byte arrays, so we need to convert a string message to a byte array
+//byte[] message = Encoding.UTF8.GetBytes("Hello from RabbitMQ .NET 6 Client!");
+//await channel.BasicPublishAsync(
+//    exchange: "",
+//    routingKey: "example-queue",
+//    body: message
+//);
+
+for(int i =0; i<100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes("Hello from RabbitMQ .NET 6 Client! " + i);
+    await channel.BasicPublishAsync(
+        exchange: "",
+        routingKey: "example-queue",
+        body: message
+    );
+}
+
+
+Console.Read();
