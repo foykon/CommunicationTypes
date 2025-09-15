048552aaaa2196dcd1d99f744c7175cc98156810
foykon <foykon9@gmail.com>
2025-09-15 20:42:52 +0300

    direct exchange usage example

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -10,38 +10,67 @@ factory.Uri = new Uri("...");
 IConnection connection = await factory.CreateConnectionAsync();
 IChannel channel = await connection.CreateChannelAsync();
 
-// declare a queue
-await channel.QueueDeclareAsync(
-    queue: "example-queue",
-    exclusive: false, // same as publisher 
-        durable: true
+channel.ExchangeDeclareAsync(
+    exchange: "direct-exchange-example",
+    type: ExchangeType.Direct
+    );
 
-);
+// get queue name
+var queueName = channel.QueueDeclareAsync().Result.QueueName;
 
-// reading message from mq
+// bind queue to exchange with routing key
+channel.QueueBindAsync(
+    queue: queueName,
+    exchange: "direct-exchange-example",
+    routingKey: "direct-queue-example"
+    );
+
+
 AsyncEventingBasicConsumer consumer = new AsyncEventingBasicConsumer(channel);
-await channel.BasicConsumeAsync(
-    queue: "example-queue",
-    false,
-    consumer
-);
-channel.BasicQosAsync(0, 1, false);
+channel.BasicConsumeAsync(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer
+    );  
+
 consumer.ReceivedAsync += (sender, e) =>
 {
-    // message from queue 
-    // e.body : gets message from queue s all data
-    // e.Body.Span or e.body.ToArray() : gets byte data
-    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
-
-    // success feedback to delete from queue
-    channel.BasicAckAsync(e.DeliveryTag, multiple: false);
-
-    // to add queue again
-    //channel.BasicNackAsync(e.DeliveryTag, multiple: false,true);
-
-    // BasicCancel rejects all queue, BasicReject only rejects the message 
-
-    return Task.CompletedTask; 
+   Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+    return Task.CompletedTask;
 };
-
+/*
+    // declare a queue
+    await channel.QueueDeclareAsync(
+        queue: "example-queue",
+        exclusive: false, // same as publisher 
+            durable: true
+    
+    );
+    
+    // reading message from mq
+    AsyncEventingBasicConsumer consumer = new AsyncEventingBasicConsumer(channel);
+    await channel.BasicConsumeAsync(
+        queue: "example-queue",
+        false,
+        consumer
+    );
+    channel.BasicQosAsync(0, 1, false);
+    consumer.ReceivedAsync += (sender, e) =>
+    {
+        // message from queue 
+        // e.body : gets message from queue s all data
+        // e.Body.Span or e.body.ToArray() : gets byte data
+        Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+    
+        // success feedback to delete from queue
+        channel.BasicAckAsync(e.DeliveryTag, multiple: false);
+    
+        // to add queue again
+        //channel.BasicNackAsync(e.DeliveryTag, multiple: false,true);
+    
+        // BasicCancel rejects all queue, BasicReject only rejects the message 
+    
+        return Task.CompletedTask; 
+    };
+*/
 Console.ReadLine();
--- Publisher/Program.cs
+++ Publisher/Program.cs
@@ -9,43 +9,61 @@ factory.Uri = new Uri("...");
 using IConnection connection = await factory.CreateConnectionAsync(); 
 using IChannel channel = await connection.CreateChannelAsync();
 
-// declare a queue
-await channel.QueueDeclareAsync(
-    queue: "example-queue",
-    exclusive: false,
-    durable: true
-);
-
-// publish a message
-// rabbitmq accepts byte arrays, so we need to convert a string message to a byte array
-//byte[] message = Encoding.UTF8.GetBytes("Hello from RabbitMQ .NET 6 Client!");
-//await channel.BasicPublishAsync(
-//    exchange: "",
-//    routingKey: "example-queue",
-//    body: message
-//);
-
-
-for (int i =0; i<100; i++)
-{
-    await Task.Delay(500);
-    Console.WriteLine(i);
-
-    var props = new BasicProperties
-    {
-        Persistent = true,
-        ContentType = "text/plain"
-    };
-
-    byte[] message = Encoding.UTF8.GetBytes("Hello from RabbitMQ .NET 6 Client! " + i);
-    await channel.BasicPublishAsync(
-        exchange: "",
-        routingKey: "example-queue",
-        mandatory: true,
-        basicProperties: props,
-        body: message
+channel.ExchangeDeclareAsync(
+    exchange: "direct-exchange-example",
+    type: ExchangeType.Direct
     );
-}
 
+
+while (true)
+{
+    Console.Write("Enter your message: ");
+    string message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublishAsync(
+        exchange: "direct-exchange-example",
+        routingKey: "direct-queue-example",
+        body: byteMessage
+        );
+}
+/*  
+    // declare a queue
+    await channel.QueueDeclareAsync(
+        queue: "example-queue",
+        exclusive: false,
+        durable: true
+    );
+    
+    // publish a message
+    // rabbitmq accepts byte arrays, so we need to convert a string message to a byte array
+    //byte[] message = Encoding.UTF8.GetBytes("Hello from RabbitMQ .NET 6 Client!");
+    //await channel.BasicPublishAsync(
+    //    exchange: "",
+    //    routingKey: "example-queue",
+    //    body: message
+    //);
+    
+    
+    for (int i =0; i<100; i++)
+    {
+        await Task.Delay(500);
+        Console.WriteLine(i);
+    
+        var props = new BasicProperties
+        {
+            Persistent = true,
+            ContentType = "text/plain"
+        };
+    
+        byte[] message = Encoding.UTF8.GetBytes("Hello from RabbitMQ .NET 6 Client! " + i);
+        await channel.BasicPublishAsync(
+            exchange: "",
+            routingKey: "example-queue",
+            mandatory: true,
+            basicProperties: props,
+            body: message
+        );
+    }
+*/
 
 Console.Read();
